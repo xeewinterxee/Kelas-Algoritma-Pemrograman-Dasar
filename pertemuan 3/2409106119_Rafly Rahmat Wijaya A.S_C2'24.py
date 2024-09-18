78a9f339f94c8a265dd4509257e08fa33f686bdb
xeewinterxee <raflywjy04@gmail.com>
2024-09-18 16:17:40 +0800

    Upload Pertemuan 3

---
+++ pertemuan 3/2409106119_Rafly Rahmat Wijaya A.S_C2'24.py
@@ -0,0 +1,74 @@
+# cuaca = "cerah"
+
+# if cuaca == "cerah":
+#     print("bisa keluar rumah")
+# else :
+#     print("diam dirumah saja")
+
+
+# Umur = int(input("Masukkan umur Anda : "))
+
+# # if Umur > 0 :
+# if Umur <= 10:
+#         print( "Umur Anda termasuk kategori anak-anak")
+# elif Umur <= 18:
+#         print( "Umur Anda termasuk kategori remaja")
+# elif Umur <=35:
+#         print("Umur Anda termasuk kategori dewasa")
+# elif Umur <=65:
+#         print("Umur Anda termasuk kategori paruh baya")
+# else:
+#         print("Umur Anda termasuk kategori Tua")
+    
+# else:
+#     print ("input usia harus bilangan positif")
+
+# nim = int(input("masukkan nim :"))
+
+# if nim >= 1 and nim <= 50 :
+#     if nim >=1 and nim <= 25:
+#         print("kelas a1")
+#     else : 
+#         print("kelas a2")
+
+# elif nim >= 51 and nim <= 100:
+#     if nim >= 51 and nim <= 75:
+#         print("kelas b1")
+#     else :
+#         print("kelas b2")
+        
+# elif nim >= 101 and nim <= 120:
+#     if nim >= 101 and nim <= 110:
+#         print("kelas c1")
+#     else :
+#         print("kelas c2")
+
+# else :
+#     print("anda bukan mahasiswa")
+
+# angka = int(input("Masukkan angka: "))
+# result = "Genap" if angka % 2 == 0 else "Ganjil"
+# print(angka, "adalah angka",result)
+
+# nim = int(input("Masukkan Nim : "))
+# hasil = "kelas a" if nim >= 1 and nim <= 50 else "kelas b" if nim >=51 and nim <= 100 else "kelas c" if nim >= 101 and nim <= 120 else "mahasiswa siluman"
+# print(hasil)
+
+
+# studi kasus 1
+
+# membeli_buku = int(input("Mau membeli berapa buku?"))
+# buku = 25000
+# diskon = 0.20
+
+# if membeli_buku >= 1 and membeli_buku <= 100:
+#     if membeli_buku >= 5 and membeli_buku <= 100:
+#         harga_buku = membeli_buku * buku 
+#         hitung_diskon = harga_buku * diskon
+#         hasil_diskon = harga_buku - hitung_diskon
+#         print(hasil_diskon)
+#     else :
+#         harga_buku = membeli_buku * buku
+#         print(harga_buku)
+# else :
+#     print("stok tidak tersedia")
