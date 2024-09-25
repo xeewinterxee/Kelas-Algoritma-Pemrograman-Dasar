c33caf305b8de4d8694e16cb98dfef66482782b1
xeewinterxee <raflywjy04@gmail.com>
2024-09-25 09:14:19 +0100

    pertemuan 4

---
+++ pertemuan 4/pertemuan4.py
@@ -0,0 +1,55 @@
+# ulang = 10
+# for i in range(ulang):
+#     print("perulangan ke-" + str(i))
+    
+# for i in range(2,10):
+#     print(f"halo {i}")
+
+# for i in range (2,10,2):
+#     print(f"halo {i}")
+    
+# for i in range (10,2,-1):
+#     print(f"halo {i}")
+
+# for i in range(1, 4): 
+#     for j in range(1, 4): 
+#         print(f"{i} x {j} = {i * j}") 
+#     print()
+
+# jawab = 'ya' 
+# hitung = 0 
+# while(jawab == 'ya'): 
+#     hitung += 1 
+#     jawab = input("Ulang lagi tidak? ") 
+#     print(f"Total perulangan: {hitung}")
+
+# hitung = 0 
+# while True: 
+#     hitung += 1 
+#     ulang = input("Masih Ingin Mengulang? ") 
+#     if ulang == "tidak" or ulang =="Tidak": 
+#         break 
+#     print(f"Total Perulangan: {hitung}")
+
+# while True:
+#     print("hallo")
+        # break
+        
+# while True:
+#     print("hallo")
+#     print("fufufufafa")
+#     continue
+
+# print("Daftar bilangan ganjil dari 1-10") 
+# for i in range(10): 
+#     if i % 2 == 0: 
+#         continue 
+#     print(i)
+
+# kelasC = ["rafly",2409106119]
+
+# for i in range(len(kelasC())):
+#     print(f"{i}. {kelasC[i]}")
+#     print(kelasC)
+
+
